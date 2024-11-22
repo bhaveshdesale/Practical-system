be0744e801e37eee2d798e77b3c40f52fa1ec0b7
Bhavesh <143746617+bhaveshdesale@users.noreply.github.com>
2024-11-22 17:28:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ app.use(express.json());
 // Routes
 app.use("/api/v1", practicalManagementRoute);
 
-app.get("/", (req, res) => {
-  res.send("Practical Management System");
-});
+app.get("/", (req, res)=>{
+  res.json({
+    success: true,
+    message: "Server Running Successfully"
+  })
+})
 
 // Export as a serverless function
 export default app;
