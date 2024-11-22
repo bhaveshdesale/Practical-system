bc2dab0cb5bbd7089fc79a671226639ea6ae9652
Bhavesh <143746617+bhaveshdesale@users.noreply.github.com>
2024-11-22 12:53:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,22 @@ import dotenv from 'dotenv';
 import dbConnect from '../config/database.js';
 import practicalManagementRoute from '../routes/practicalManagementRoute.js';
 
-
 dotenv.config();
 
-const app=express();
+const app = express();
 
-
+// Connect to the database
 dbConnect();
 
+// Middleware
 app.use(express.json());
 
-app.use("/api/v1",practicalManagementRoute);
+// Routes
+app.use("/api/v1", practicalManagementRoute);
 
-app.get("/",(req,res)=>{
-    res.send("Practical Management System");
-})
-const PORT=3000 || process.env.PORT ;
-app.listen(PORT,()=>{
-    console.log(`sever is running at port: ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Practical Management System");
+});
 
-})
-
+// Export as a serverless function
+export default app;
